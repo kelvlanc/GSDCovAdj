1519a46268dba8d821101f98f2b2ebcb5edf18b7
kelvlanc <kelly.vanlancker@ugent.be>
2022-07-20 19:12:46 -0400

    Add documentation for tmle

--- R/tmle.R
+++ R/tmle.R
@@ -2,23 +2,23 @@
 #'
 #' @description A function that estimates the treatment effect via a (longitudinal) targeted maximum likelihood estimator.
 #'
-#' @param data A data frame containing all observed data for the current analysis
-#' @param estimand Estimand of interest; treatment effect on "difference" scale, "ratio" scale or odds ratio ("oddsratio") scale
-#' @param propensity_score_formula TODO
-#' @param inverse_weight_formulas TODO
-#' @param outcome_formulas TODO
-#' @param outcome_type TODO
-#' @param outcome_range TODO
-#' @param absorbing_state TODO
-#' @param absorbing_outcome TODO
-#' @param impute_formulas TODO
-#' @param impute_model TODO
-#' @param imputation_args TODO
-#' @param ci TODO
-#' @param verbose TODO
-#' @param bootstrap_n TODO
-#' @param bootstrap_type TODO
-#' @param alpha TODO
+#' @param data A data frame containing the observed data at a given time. This data frame should have the same structure as the data frame outputted by \code{data_at_time_t}.
+#' @param estimand A character string indicating the estimand of interest; treatment effect on "difference" scale, "ratio" scale or odds ratio ("oddsratio") scale.
+#' @param propensity_score_formula A formula for the propensity score model - the left-hand-side indicates the binary treatment assignment indicator variable.
+#' @param inverse_weight_formulas A list of formulas for computing inverse probability of censoring weights. Only the right-hand side of the formula is used.
+#' @param outcome_formulas A list of formulas with one formula per outcome model, ordered from the first to the last observed outcome.
+#' @param outcome_type A character string indicating the type of outcome model to be fitted: "gaussian", "logistic", "binomial", "multinomial-binomial".
+#' @param outcome_range A numeric vector of length 2 indicating the range of the outcome. Only used when outcome_type == "logistic".
+#' @param absorbing_state A character vector indicating which level (if any) of the outcome is an absorbing state. Only used when outcome_type == "multinomial-binomial".
+#' @param absorbing_outcome A value of 0 or 1 denoting whether the absorbing state is a success (1) or failure (0) for the binary final outcome. Only used when outcome_type == "multinomial-binomial".
+#' @param impute_formulas A list of formulas with one formula per intermediate outcome, containing the paramaterization of imputation models for intermediate outcomes.
+#' @param impute_model A character vector indicating the family of imputation model to use: "gaussian" (default), "binomial", "beta", "pmm", "multinomial".
+#' @param imputation_args A list of arguments to pass to functions performing imputation.
+#' @param ci TRUE/FALSE - Should bootstrap-based confidence intervals be returned (TRUE) or just the estimate (FALSE).
+#' @param verbose TRUE/FALSE - Should all models and intermediate results be returned (TRUE) or just the estimate (FALSE).
+#' @param bootstrap_n Number of bootstrap replicates used to perform bootstrap inference.
+#' @param bootstrap_type The type of bootstrap to perform: "bca" (default), "norm", "basic", or "perc".
+#' @param alpha Type I Error rate used in the confidence level for bootstrap inference - Defaults to 0.05.
 #' @param ... Further arguments for the estimator function.
 #'
 #' @importFrom stats glm predict
